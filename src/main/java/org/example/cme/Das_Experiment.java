2649719b3ca8208425674f3171a4af68a67ae34d
Anatoli_Aniskevich <a.aniskevich@eis-partners.com>
2023-04-20 20:33:37 +0400

    first commit

--- src/main/java/org/example/cme/Das_Experiment.java
+++ src/main/java/org/example/cme/Das_Experiment.java
@@ -1,1 +1,31 @@
-package org.example.cme;import java.util.ConcurrentModificationException;import java.util.Iterator;import java.util.Map;public class Das_Experiment {    private static final Object object = new Object();    public static void main(String[] args) {        Map<Integer, Integer> map = new ThreadSafeMap<>();        map.put(1, 1);        Iterator<Integer> it = map.values().iterator();        System.out.println(it.next());        map.put(2, 2);        try {            System.out.println(it.next());            map.put(3,3);            System.out.println(it.next());        } catch (ConcurrentModificationException e) {            e.printStackTrace();        }    }}
+package org.example.cme;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Das_Experiment {
+
+    private static final Object object = new Object();
+
+    public static void main(String[] args) {
+
+        Map<Integer, Integer> map = new ThreadSafeMap<>();
+
+        map.put(1, 1);
+
+        Iterator<Integer> it = map.values().iterator();
+
+        System.out.println(it.next());
+
+        map.put(2, 2);
+
+        try {
+            System.out.println(it.next());
+            map.put(3,3);
+            System.out.println(it.next());
+        } catch (ConcurrentModificationException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/example/cme/ThreadSafeMap.java
+++ src/main/java/org/example/cme/ThreadSafeMap.java
@@ -1,1 +1,77 @@
-package org.example.cme;import java.io.Serializable;import java.util.Collection;import java.util.HashSet;import java.util.Map;import java.util.Set;public class ThreadSafeMap<K, V>    implements Map<K, V>, Cloneable, Serializable {    private final Object lock = new Object();    private CustomSet<V> customSet = new CustomSet<>();    @Override    public V put(K key, V value) {        synchronized (lock) {            customSet.add(value);        }        return value;    }    @Override    public Collection<V> values() {        return customSet;    }    @Override    public Set<K> keySet() {        return new HashSet<>();    }    @Override    public int size() {        return customSet.size();    }    @Override    public boolean isEmpty() {        return false;    }    @Override    public boolean containsKey(Object key) {        return false;    }    @Override    public boolean containsValue(Object value) {        return false;    }    @Override    public V get(Object key) {        return null;    }    @Override    public V remove(Object key) {        return null;    }    @Override    public void putAll(Map<? extends K, ? extends V> m) {    }    @Override    public void clear() {    }    @Override    public Set<Entry<K, V>> entrySet() {        return null;    }}
+package org.example.cme;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ThreadSafeMap<K, V>
+    implements Map<K, V>, Cloneable, Serializable {
+
+    private final Object lock = new Object();
+    private CustomSet<V> customSet = new CustomSet<>();
+
+    @Override
+    public V put(K key, V value) {
+        synchronized (lock) {
+            customSet.add(value);
+        }
+        return value;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return customSet;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public int size() {
+        return customSet.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
